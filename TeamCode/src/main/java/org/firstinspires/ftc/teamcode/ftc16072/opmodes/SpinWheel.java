c5ee03948a4e7b71907d0727ebca25bbc3a04933
preetithirukonda <preetithirukonda@gmail.com>
2023-01-07 14:53:08 -0500

    wrote code for spinny thing, not tested yet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/opmodes/SpinWheel.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.ftc16072.opmodes;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.WheelMotor;
+
+@TeleOp()
+public class SpinWheel extends OpMode {
+    public WheelMotor wheel = new WheelMotor();
+
+    @Override
+    public void init() {
+        wheel.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        wheel.spin();
+        telemetry.addData("position: ",wheel.getPosition());
+
+    }
+}
