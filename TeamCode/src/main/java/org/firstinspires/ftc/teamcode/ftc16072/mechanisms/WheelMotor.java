693a71406a48d1aeb593950de0050ff0f23d7187
EastZeus <swayamshah244@gmail.com>
2023-01-28 18:08:30 -0500

    wheel power increased

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/WheelMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/WheelMotor.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class WheelMotor extends Mechanism {
+    public static final double SPIN_SPEED = .5;
     public DcMotor wheelMotor;
     public TouchSensor button;
     private static int currentSpin;
@@ -31,28 +32,28 @@ public class WheelMotor extends Mechanism {
         if (position()==1) {
             wheelMotor.setTargetPosition(TEAM_PLAN);
             wheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            wheelMotor.setPower(.2);
+            wheelMotor.setPower(SPIN_SPEED);
         }else if (position()==2) {
-            wheelMotor.setTargetPosition(EXPERTS);
-            wheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            wheelMotor.setPower(.2);
-
-        }else if (position()==3) {
-            wheelMotor.setTargetPosition(OUTREACH);
-            wheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            wheelMotor.setPower(.2);
-        }else if (position()==4) {
             wheelMotor.setTargetPosition(GOALS);
             wheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            wheelMotor.setPower(.2);
+            wheelMotor.setPower(SPIN_SPEED);
+
+        }else if (position()==3) {
+            wheelMotor.setTargetPosition(EXPERTS);
+            wheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wheelMotor.setPower(SPIN_SPEED);
+        }else if (position()==4) {
+            wheelMotor.setTargetPosition(OUTREACH);
+            wheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wheelMotor.setPower(SPIN_SPEED);
         }else if (position()==5) {
             wheelMotor.setTargetPosition(DESIGN_PROCESS);
             wheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            wheelMotor.setPower(.2);
+            wheelMotor.setPower(SPIN_SPEED);
         }else if (position()==6) {
             wheelMotor.setTargetPosition(CONTROL_CODE);
             wheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            wheelMotor.setPower(.2);
+            wheelMotor.setPower(SPIN_SPEED);
         }else {
             wheelMotor.setPower(0);
         }
